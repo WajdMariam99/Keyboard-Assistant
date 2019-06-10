07992966a23faf2993142e6a844249b5cabf5a96
WajdMariam99 <47641469+WajdMariam99@users.noreply.github.com>
2019-06-10 13:25:19 -0400

    Update main.lua

--- Keyboard Assistant Part(A)/main.lua
+++ Keyboard Assistant Part(A)/main.lua
@@ -7,18 +7,37 @@
 display.setDefault ("background", 255/255, 255/255, 255/255 )
 
 
-local Keyboard = display.newImageRect ("assets/sprites/keyboard.png", 2500, 1500 )
+local Keyboard = display.newImageRect ("assets/sprites/keyboard.png", 450, 250 )
 Keyboard.x = display.contentCenterX 
 Keyboard.y = display.contentCenterY
 Keyboard.id = "Keyboard"
 
 
-local Title = display.newText ("Keyboard Assistant", display.contentCenterX , display.contentCenterY - 900, native.systemFont, 100)
+local Title = display.newText ("Keyboard Assistant", display.contentCenterX , display.contentCenterY - 150 , native.systemFont, 30)
 Title:setFillColor (0/255,0/255,0/255)
 
 
-local name  = display.newText ("Hi! What's your name?",display.contentCenterX - 150, display.contentCenterY + 800, native.systemFont, 100 )
+local name  = display.newText ("Hi! What's your name?",display.contentCenterX - 85, display.contentCenterY + 145, native.systemFont, 20 )
 name:setFillColor (0/255,0/255,0/255)
 
 
-local nameTextfield  = native.newTextField ( display.contentCenterX + 700, display.contentCenterY + 800, 450 , 150)
+local nameTextfield  = native.newTextField ( display.contentCenterX + 75 , display.contentCenterY + 145  , 85 , 30)
+nameTextfield.id = "Name TextField"
+
+
+local EnterButton = display.newImageRect ("assets/sprites/enterButton.png", 85 , 50)
+EnterButton.x = display.contentCenterX + 175
+EnterButton.y = display.contentCenterY + 145
+EnterButton.id = "enterButton"
+
+local square = display.newImageRect ("assets/sprites/square.png", 30.1, 31.7)
+square.x = 95.5
+square.y = 119.1
+square.id = "square"
+
+
+t = {}
+
+t[1] = {x , 119.1}
+t[1].id = "49"
+
